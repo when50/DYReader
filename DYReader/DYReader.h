375a298b628099034217a8c96faef11f53abb2fa
cuihaicheng <cuihaicheng@aggrx.com>
2022-06-30 16:29:03 +0800

    Initial Commit

---
+++ DYReader/DYReader.h
@@ -0,0 +1,18 @@
+//
+//  DYReader.h
+//  DYReader
+//
+//  Created by oneko on 2022/6/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DYReader.
+FOUNDATION_EXPORT double DYReaderVersionNumber;
+
+//! Project version string for DYReader.
+FOUNDATION_EXPORT const unsigned char DYReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DYReader/PublicHeader.h>
+
+
